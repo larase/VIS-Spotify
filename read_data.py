7e0e6ed3691a22cbcee580e060c986f24327c7ed
OwlWithACupOfTea <91909396+OwlWithACupOfTea@users.noreply.github.com>
2022-11-20 00:43:22 +0100

    Daten einlesbar (MIT Panda)

--- read_data.py
+++ read_data.py
@@ -1,4 +1,10 @@
-with open("top10s.csv", "r") as f:
+"""with open("top10s.csv", "r") as f:
     read_data = f.read()
 
-print(read_data)
+print(read_data)"""
+
+import pandas as pd
+
+df = pd.read_csv("top10s.csv", encoding = 'unicode_escape', engine ='python')
+
+print(df)
