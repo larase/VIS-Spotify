fdaa076e05d9354bfe885c09cae41ec83afba94f
OwlWithACupOfTea <91909396+OwlWithACupOfTea@users.noreply.github.com>
2022-11-20 09:31:10 +0100

    Template Aufgabe 11b) hinzugfügt

--- read_data.py
+++ read_data.py
@@ -1,10 +1,19 @@
-"""with open("top10s.csv", "r") as f:
-    read_data = f.read()
-
-print(read_data)"""
-
 import pandas as pd
 
 df = pd.read_csv("top10s.csv", encoding = 'unicode_escape', engine ='python')
 
 print(df)
+
+"""
+Aufgabe 11b Template
+
+import numpy as np
+
+list = df['pop']
+
+print(np.mean(list))
+print(np.std(list))
+print(np.var(list))
+print(np.median(list))
+print(np.max(list))
+"""
